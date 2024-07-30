9cd122ba50d86ffb921441e8dcf1096d537a86b4
jiyun <ranji12@naver.com>
2024-07-30 23:20:27 +0900

    정적으로 파일 가져오는 부분에서 에러나는 것 같음

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+import express, {Request, Response} from 'express'
 import path from 'path';
+const app = express()
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.post('/send',(req : Request ,res:Response)=>{
+  const data = req.body;
+  console.log(data)
+  res.json({message:"성공적"})
+})
 
 app.listen(8080, () => {
     console.log(`Server is running at http://localhost:8080`);
