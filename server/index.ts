dfab2cc5063fd48015ef7b1d2e6a1592f3627c15
신지윤 <ranji12@naver.com>
2024-08-03 23:33:06 +0900

    import path변경

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-import path from 'path';
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/dist')))
 
