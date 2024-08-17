1174450c1bec6c2e3e32e6eeaeb55493ad13aca5
신지윤 <ranji12@naver.com>
2024-08-17 22:58:30 +0900

    카드 페이지

---
+++ client/src/main/taromain.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+//DTO(카드 내용)
+const TaroMain: React.FC = () => {
+    const [state, setState] = useState('');
+return (
+    <div>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+    </div>
+
+)
+};
+export default TaroMain;
