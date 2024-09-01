d089e8bca5325d49556a478ce18e6e6dc0a1b5e5
신지윤 <ranji12@naver.com>
2024-09-01 23:05:13 +0900

    카드 css추가 후 파일확장자 tsx->ts로 변경

---
+++ client/src/style/styles.ts
@@ -0,0 +1,48 @@
+import React from 'react';
+const styles: {[key:string]:React.CSSProperties} = {
+  content : {
+    display:"flex",
+    flexDirection:"column",
+    height:"100vh",
+    width:"100vw",
+  },
+  contentHead:{
+    textAlign: "center",
+    // display:"flex"
+  },
+  allCard : {
+    display: 'flex',
+    marginLeft:'20px',
+    height: '100vh',
+    width: '100vw',
+    alignItems:'center',
+    justifyContent:'center',
+    gap:'30px'
+  },
+  oneCard : {
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    width: '10vw',
+    height: '20vw',
+    backgroundColor: 'lightpink'
+  },
+  twoCard : {
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    width: '10vw',
+    height: '20vw',
+    backgroundColor: 'lightblue'
+  },
+  threeCard : {
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+    width: '10vw',
+    height: '20vw',
+    backgroundColor: 'lightgreen'
+  },
+};
+
+export default styles
