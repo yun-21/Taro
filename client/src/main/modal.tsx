90defef0cd80faaa1541e7845c843938a43e8008
신지윤 <ranji12@naver.com>
2024-09-02 16:52:34 +0900

    점수로 인한 오늘 하루 운세 변경

--- client/src/main/modal.tsx
+++ client/src/main/modal.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 type ModalProps = {
   modalState: boolean;
   onClose: () => void;
+  score: number;
 };
 
 // Modal 컴포넌트 정의
-const Modal: React.FC<ModalProps> = ({ modalState, onClose }) => {
+const Modal: React.FC<ModalProps> = ({ modalState, onClose, score }) => {
   if (!modalState) {
     return null; // show가 false일 때는 아무것도 렌더링하지 않음
   }
 
   function randomTaro(): string {
-    const score = 80;
     if (score >= 80) {
       const succes = ["오늘은 먹을 복이 있으신 것 깉습니다.", "빨간색 구두가 행운을 불러올 것 같습니다.", "모자가 오늘의 행운의 아이템입니다.", "달을 보면 좋은 일이.."];
       const randomIndex = Math.floor(Math.random() * succes.length);
