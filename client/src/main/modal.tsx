974f79fc907d8ae47f33e03329d8d8ae64916026
신지윤 <ranji12@naver.com>
2024-09-02 12:39:42 +0900

    돌 바구니에 넣은 점수로 운세를 높인다.

--- client/src/main/modal.tsx
+++ client/src/main/modal.tsx
@@ -13,9 +13,16 @@ const Modal: React.FC<ModalProps> = ({ modalState, onClose }) => {
   }
 
   function randomTaro(): string {
-    const result = ["오늘은 먹을 복이 있으신 것 깉습니다.", "빨간색 구두가 행운을 불러올 것 같습니다.", "모자가 오늘의 행운의 아이템입니다.", "달을 보면 좋은 일이.."];
-    const randomIndex = Math.floor(Math.random() * result.length);
-    return result[randomIndex];
+    const score = 80;
+    if (score >= 80) {
+      const succes = ["오늘은 먹을 복이 있으신 것 깉습니다.", "빨간색 구두가 행운을 불러올 것 같습니다.", "모자가 오늘의 행운의 아이템입니다.", "달을 보면 좋은 일이.."];
+      const randomIndex = Math.floor(Math.random() * succes.length);
+      return succes[randomIndex];
+    } else {
+      const fail = ["오늘은 물도 조심히 마셔야겠습니다.", "건강을 유의하십시오.", "피곤함이 많이 몰려올 것 같습니다.", "금으로 된 물건을 지니고 다니시면 불운이 사라질 것 같습니다."]
+      const randomIndex = Math.floor(Math.random() * fail.length);
+      return fail[randomIndex];
+    }
   }
 
   return (
