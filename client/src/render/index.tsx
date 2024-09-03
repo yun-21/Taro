1a7eb4796b4cd694f97a0c774e539dc1d551ad19
신지윤 <ranji12@naver.com>
2024-09-03 23:54:35 +0900

    렌더링 잠시 변경

--- client/src/render/index.tsx
+++ client/src/render/index.tsx
@@ -1,4 +1,5 @@
-import App from '../main/page_router'
+// import App from '../main/page_router'
+import App from "../main/score";
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
