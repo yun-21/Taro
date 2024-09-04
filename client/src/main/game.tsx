50575a0188ec3365c0719daa7d2c9631f31064e7
신지윤 <ranji12@naver.com>
2024-09-04 11:07:02 +0900

    돌 고정 값으로 수정

--- client/src/main/game.tsx
+++ client/src/main/game.tsx
@@ -54,30 +54,69 @@
 
 // export default DropTarget;
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
 import { gameStyles } from '../style/game_style';
+import DraggableItem from './stone_component';
+import DndContext from './dbcontent';
 
 interface DragStone {
   id: string;
+  top: number;
+  left: number;
 }
 
-const DropTarget: React.FC<{onDrop: (id: string) => void}> = ({ onDrop }) => {
+const DropTarget: React.FC = () => {
+  const containerWidth = window.innerWidth * 0.7;
+  const containerHeight = window.innerHeight;
+  const stoneHeight = 40; // 돌의 높이
+  const stoneWidth = 40; // 돌의 넓이
+  // const stoneNum = 10;
+  const stones = Array.from({ length: 10 }, (_, index) => ({
+        id: `stone${index}`,
+        top: containerHeight - stoneHeight - Math.random() * 30,
+        left: containerWidth - stoneWidth - Math.random() * (containerWidth - stoneWidth),
+      }));
+  const [stone, setStone] = useState(stones);
+  const [score, setScore] = useState(0);
   const [{ isOver }, drop] = useDrop<DragStone, void, { isOver: boolean }>(() => ({
     accept: 'STONE',
     drop: (item) => {
-      onDrop(item.id);
+      dropStone(item.id);
+      setScore(getScore => getScore + 10);
     },
   }));
-  
+
+  const dropStone = (id: string) => {
+    setStone(stone => stone.filter(item => item.id !== id));
+  }
+
+  // const stoneRandom = () => {
+  //   const top = containerHeight - stoneHeight - Math.random() * 30;
+  //   const left = containerWidth - stoneWidth - Math.random() * (containerWidth - stoneWidth);
+  //   return {top, left}
+  // }
   const basketStyle: React.CSSProperties = {
     ...gameStyles.basket,
     backgroundColor: isOver ? 'lightgreen' : 'lightgray'
   }
 
   return (
-    <div ref={drop} style={basketStyle}>
+    <>
+      <DndContext>
+        <div>
+          {stone.map(item => (
+            // const {top, left} = stoneRandom();
+            <DraggableItem key={item.id} id={item.id} top={item.top} left={item.left} />
+            ))}
+        </div>
+      </DndContext>
+      <div ref={drop} style={basketStyle}>
+      </div>
+      <div style={{ position: 'absolute', top: 0, left: 0, padding: '10px' }}>
+      점수: {score}
     </div>
+    </>
   );
 };
 
