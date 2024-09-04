3af0ff17821828db1df3269f545e686df01e1d5d
신지윤 <ranji12@naver.com>
2024-09-04 15:42:08 +0900

    게임 설명에 점수에 따른 운세 퍼센트 안내

--- client/src/main/gameStart.tsx
+++ client/src/main/gameStart.tsx
@@ -16,7 +16,15 @@ const GameStartPage = () => {
     <>
       <div>돌 빨리 넣기 게임</div>
       <div onMouseOver={mouseOver} onMouseOut={mouseOut} style={{color:`${color}`}}>게임설명</div>
-      <div style={{display:`${display}`}}>gdgd</div>
+      <div style={{display:`${display}`}}>
+        <p>10개의 돌을 주워서 바구니에 넣는 게임이다!</p>
+        <p>단, 제한시간 6초!</p>
+        <p>돌 하나 당 10점을 획득한다. 권투를 빌겠다.</p>
+        <p>하수 (점수 30점 이하) : 운세가 오르지 않는다.</p>
+        <p>중수 (점수 50점 이하) : 운세가 10% 올랐다.</p>
+        <p>고수 (점수 70점 이하) : 운세가 40% 올랐다.</p>
+        <p>초고수 (점수 80점 이상) : 운세가 100% 올랐다.</p>
+      </div>
       <Link to={'/game'}>
         <div>시작하기</div>
       </Link>
