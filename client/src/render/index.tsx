554d77eb1d29d3a2eb1b3860971a46d44df75e62
신지윤 <ranji12@naver.com>
2024-09-04 14:46:45 +0900

    경로 재설정

--- client/src/render/index.tsx
+++ client/src/render/index.tsx
@@ -1,5 +1,4 @@
-// import App from '../main/page_router'
-import App from "../main/score";
+import App from '../main/page_router'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
