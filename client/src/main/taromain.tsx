f33207bd1606b1a865873c92dc01f1d147f5c099
신지윤 <ranji12@naver.com>
2024-09-04 14:52:01 +0900

    운세 메인 페이지 운세 올리기 게임 link변경

--- client/src/main/taromain.tsx
+++ client/src/main/taromain.tsx
@@ -34,17 +34,9 @@ return (
             <div>
             </div>
             <div>
-                <Link to="/game">
-                    운세 올리기
+                <Link to="/gameStart">
+                    운세 올리기 게임
                 </Link>
-            </div>
-            <div style={styles.imsiStyle}>
-                <div>
-                    현재 게임이 안되는 관계로 밑에다가 숫자 80미만 혹은 80이상을 적으시면 운세가 달라집니다.
-                </div>
-                <div>
-                    <input type="text" value={score} onChange={scoreChange} placeholder='게임이안됨 점수작성해주세요(1~100까지)'/>
-                </div>
             </div>
             <Modal modalState={modalState} onClose={initModal} score={score}/>
         </div>
