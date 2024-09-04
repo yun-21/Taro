1c5be5aee2303077b058e9c8d165f0a634d15e98
신지윤 <ranji12@naver.com>
2024-09-04 15:41:47 +0900

    게임 제한시간 추가

--- client/src/main/game.tsx
+++ client/src/main/game.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDrop } from 'react-dnd';
 import { gameStyles } from '../style/game_style';
 import DraggableItem from './stone_component';
+import { Link } from 'react-router-dom';
 
 interface DragStone {
   id: string;
@@ -22,6 +23,7 @@ const DropTarget: React.FC = () => {
       }));
   const [stone, setStone] = useState(stones);
   const [score, setScore] = useState(0);
+  const [gameActive, setGameActive] = useState(true);
   const [{ isOver }, drop] = useDrop<DragStone, void, { isOver: boolean }>(() => ({
     accept: 'STONE',
     drop: (item) => {
@@ -42,18 +44,51 @@ const DropTarget: React.FC = () => {
     backgroundColor: isOver ? '#C5965E' : '#996600'
   }
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setGameActive(false);
+    }, 6000);
+
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, []);
+
+  const scoreMsg = () => {
+    if (score <= 30) {
+      return '...하수에겐 할 말이 없네요';
+    } else if (score <= 50) {
+      return '손이 그럭저럭 빠른가보네요? 칭찬해~';
+    } else if (score <= 70 ){
+      return '꽤 하네? 조금만 더 했더라면~ 초고수가 될 수도 있었는데 말이지..';
+    } else {
+      return '당신... 초고수잖아? 대단해.. 운세를 100% 올려주도록 하지..';
+    }
+  };
   return (
     <>
-      <div>
-        {stone.map(item => (
-          <DraggableItem key={item.id} id={item.id} top={item.top} left={item.left} />
-        ))}
-      </div>
-      <div ref={drop} style={basketStyle}>
-      </div>
-      <div style={{ position: 'absolute', top: 0, left: 0, padding: '10px' }}>
-      점수: {score}
-    </div>
+      {gameActive ?
+        <>
+          <div>
+            {stone.map(item => (
+              <DraggableItem key={item.id} id={item.id} top={item.top} left={item.left} />
+            ))}
+          </div>
+          <div ref={drop} style={basketStyle}></div>
+        
+          <div style={{ position: 'absolute', top: 0, left: 0, padding: '10px' }}>점수: {score}</div>
+        </>
+        :
+        <div>
+          <h1>게임종료</h1>
+          <div>당신의 점수는 <b>{score}점</b></div>
+          <div>{scoreMsg()}</div>
+          <Link to='/'>
+            <p>운세 보러가기</p>
+          </Link>
+          <Link to='/gameStart'>
+            <p>게임 다시하기</p>
+          </Link>
+        </div>
+      }
     </>
   );
 };
