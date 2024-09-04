a8bcc3dccde160116f153e68a33579181a4a8ece
신지윤 <ranji12@naver.com>
2024-09-04 11:39:28 +0900

    바구니에 돌 올려두면 색깔 변하도록 설정(바구니에 잘 올라갔나 확인할 수 있도록)

--- client/src/main/game.tsx
+++ client/src/main/game.tsx
@@ -1,59 +1,3 @@
-// import React, { useState } from 'react';
-// import Stone from './stone_component';
-// import { gameStyles } from '../style/game_style';
-
-// const Game: React.FC = () => {
-//   const [score, setScore] = useState(0);
-//   const [numberOfStones] = useState(10);
-//   // 돌을 동적으로 생성
-//   const stones = Array.from({ length: numberOfStones }, (_, index) => {
-//     return <Stone key={index}/>;
-//   });
-//   return (
-//     <div style={gameStyles.allContent}>
-//       <div style={gameStyles.stoneContent}>
-//         {stones}
-//       </div>
-//       <div style={gameStyles.basketContent}>
-//         <div style={gameStyles.basket}></div>
-//       </div>
-//       <div style={{ position: 'absolute', top: 0, left: 0, padding: '10px' }}>
-//         점수: {score}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Game;
-
-// import { useDrop } from 'react-dnd';
-
-// interface DragItem {
-//   id: string;
-// }
-
-// const DropTarget: React.FC<{ onDrop: (id: string) => void }> = ({ onDrop }) => {
-//   const [{ isOver }, drop] = useDrop<DragItem, void, { isOver: boolean }>(() => ({
-//     accept: 'ITEM',
-//     drop: (item) => {
-//       if (item) {
-//         onDrop(item.id);
-//       }
-//     },
-//     collect: (monitor) => ({
-//       isOver: monitor.isOver(),
-//     }),
-//   }));
-
-//   return (
-//     <div ref={drop} style={{ backgroundColor: isOver ? 'lightgreen' : 'lightgray' }}>
-//       Drop here
-//     </div>
-//   );
-// };
-
-// export default DropTarget;
-
 import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
 import { gameStyles } from '../style/game_style';
@@ -85,31 +29,26 @@ const DropTarget: React.FC = () => {
       dropStone(item.id);
       setScore(getScore => getScore + 10);
     },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      // canDrop: monitor.canDrop(),
+    }),
   }));
 
   const dropStone = (id: string) => {
     setStone(stone => stone.filter(item => item.id !== id));
   }
-
-  // const stoneRandom = () => {
-  //   const top = containerHeight - stoneHeight - Math.random() * 30;
-  //   const left = containerWidth - stoneWidth - Math.random() * (containerWidth - stoneWidth);
-  //   return {top, left}
-  // }
   const basketStyle: React.CSSProperties = {
     ...gameStyles.basket,
-    backgroundColor: isOver ? 'lightgreen' : 'lightgray'
+    backgroundColor: isOver ? '#C5965E' : '#996600'
   }
 
   return (
     <>
       <DndContext>
-        <div>
-          {stone.map(item => (
-            // const {top, left} = stoneRandom();
-            <DraggableItem key={item.id} id={item.id} top={item.top} left={item.left} />
-            ))}
-        </div>
+        {stone.map(item => (
+          <DraggableItem key={item.id} id={item.id} top={item.top} left={item.left} />
+        ))}
       </DndContext>
       <div ref={drop} style={basketStyle}>
       </div>
