c73fbcd87c9a188bcecf67dffb0a150f040c514a
신지윤 <ranji12@naver.com>
2024-09-07 12:05:00 +0900

    임시 저장

--- client/src/main/modal.tsx
+++ client/src/main/modal.tsx
@@ -14,14 +14,27 @@ const Modal: React.FC<ModalProps> = ({ modalState, onClose, score }) => {
   }
 
   function randomTaro(): string {
-    if (score >= 80) {
-      const succes = ["오늘은 먹을 복이 있으신 것 깉습니다.", "빨간색 구두가 행운을 불러올 것 같습니다.", "모자가 오늘의 행운의 아이템입니다.", "달을 보면 좋은 일이.."];
+    // if (score >= 80) {
+    //   const succes = ["오늘은 먹을 복이 있으신 것 깉습니다.", "빨간색 구두가 행운을 불러올 것 같습니다.", "모자가 오늘의 행운의 아이템입니다.", "달을 보면 좋은 일이.."];
+    //   const randomIndex = Math.floor(Math.random() * succes.length);
+    //   return succes[randomIndex];
+    // } else {
+    //   const fail = ["오늘은 물도 조심히 마셔야겠습니다.", "건강을 유의하십시오.", "피곤함이 많이 몰려올 것 같습니다.", "금으로 된 물건을 지니고 다니시면 불운이 사라질 것 같습니다."]
+    //   const randomIndex = Math.floor(Math.random() * fail.length);
+    //   return fail[randomIndex];
+    // }
+    if(score <= 30) {
+      const succes = ['크게 다칠 수도 있습니다.', '관절을 조심하세요.', ''];
+      const randomIndex = Math.floor(Math.random() * succes.length);
+      return succes[randomIndex];
+    } else if (score <= 50) {
+      const succes = ['50점'];
       const randomIndex = Math.floor(Math.random() * succes.length);
       return succes[randomIndex];
     } else {
-      const fail = ["오늘은 물도 조심히 마셔야겠습니다.", "건강을 유의하십시오.", "피곤함이 많이 몰려올 것 같습니다.", "금으로 된 물건을 지니고 다니시면 불운이 사라질 것 같습니다."]
-      const randomIndex = Math.floor(Math.random() * fail.length);
-      return fail[randomIndex];
+      const succes = ['100점'];
+      const randomIndex = Math.floor(Math.random() * succes.length);
+      return succes[randomIndex];
     }
   }
 
