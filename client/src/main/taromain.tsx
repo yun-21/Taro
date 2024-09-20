8c9d37d7093b60bca9eb8f15c404d5e99bd81e13
신지윤 <ranji12@naver.com>
2024-09-20 12:39:44 +0900

    선택하는 카드마다 다른 운세배열을 위한 설정

--- client/src/main/taromain.tsx
+++ client/src/main/taromain.tsx
@@ -9,7 +9,10 @@ const TaroMain: React.FC = () => {
     const [modalState, setModalState] = useState(false);
     const queryParams = new URLSearchParams(location.search);
     const score = Number(queryParams.get('score'));
-    const setModal = () => {
+    const [selectedCard, setSelectedCard] = useState<string>(''); // 클릭한 카드 저장
+
+    const setModal = (cardType: string) => {
+        setSelectedCard(cardType);
         setModalState(true);
     }
     const initModal = () => {
@@ -22,18 +25,18 @@ return (
             오늘의 운세
         </h1>
         <div style={styles.allCard}>
-            <div onClick={setModal}>
+            <div onClick={()=> setModal('oneCard')}>
                 <div style={styles.oneCard}>1</div>
             </div>
-            <div onClick={setModal}>
+            <div onClick={()=> setModal('twoCard')}>
                 <div style={styles.twoCard}>2</div>
             </div>
-            <div onClick={setModal}>
+            <div onClick={()=> setModal('threeCard')}>
                 <div style={styles.threeCard}>3</div>
             </div>
             <div>
             </div>
-            <Modal modalState={modalState} onClose={initModal} score={score}/>
+            <Modal modalState={modalState} onClose={initModal} score={score} selectedCard={selectedCard} />
         </div>
         <div style={styles.footer}>
             <Link to="/gameStart">
